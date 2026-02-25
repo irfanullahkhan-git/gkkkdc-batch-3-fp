aed5eae8e9bd6c7d2ab90ae80ba2798a780ae038
Irfanullah Khan <irfanullah.khan@jintech.com>
2026-02-25 22:54:47 +0500

    IK - 25/02/2026

---
+++ 7.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1, num2 = 10;
+	int num = 20;
+	
+	num1 = 30;
+	
+	int result = num1 + num2;
+	cout<<"Sum: "<<result<<endl;
+	
+	result = num1 - num2;
+	cout<<"Difference: "<<result<<endl;
+	
+	result = num1 * num2;
+	cout<<"Product: "<<result<<endl;
+
+	result = num1 / num2;
+	cout<<"Ratio: "<<result<<endl;
+	
+	num1 = 14, num2 = 4;
+	result = num1 % num2;
+	cout<<"Remainder: "<<result<<endl;
+	
+	num1 = 4, num2 = 4;
+	result = num1 % num2;
+	cout<<"Remainder: "<<result<<endl;
+	
+	num1 = 3, num2 = 4;
+	result = num1 / num2;
+	cout<<"Remainder: "<<result<<endl;
+	
+	
+	float f1 = 3, f2 = 4, f3;
+	f3 = f1 / f2;
+	cout<<"Ratio: "<<f3<<endl;
+	
+		
+	
+	
+}
