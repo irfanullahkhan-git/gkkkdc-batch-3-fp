348add2a67d3f3b685b00b7bd7adbb4ae8627685
Irfanullah Khan <irfanullah.khan@jintech.com>
2026-03-04 12:58:57 +0500

    IK - 04/03/2026

--- 18.cpp
+++ 18.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int main(){
 	int n1 = 10;
 	int n2 = 138;
-	int n3;
 	
-	n3 = n1;
-	n1 = n2;
-	n2 = n3;
-	
-	
+	n1 = n1 + n2; // 148
+	n2 = n1 - n2; // 10
+	n1 = n1 - n2; // 138
 	
 	cout<<n1<<endl; // 138
 	cout<<n2<<endl; // 10
